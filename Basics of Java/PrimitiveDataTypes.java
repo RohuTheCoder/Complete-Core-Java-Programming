910c7894bbe75c5da52b36d40951628c8d88def0
Rohini G B <gbrohini840@gmail.com>
2024-11-09 17:07:22 +0530

    Basics of Java

---
+++ Basics of Java/PrimitiveDataTypes.java
@@ -0,0 +1,36 @@
+public class PrimitiveDataTypes
+{
+    public static void main(String[] args) {
+        //byte type
+        byte b = 20;
+        System.out.println("b = "+b);
+
+        //short type
+        short s = 20;
+        System.out.println("s = "+s);
+
+        //int type
+        int i = 20;
+        System.out.println("i = "+i);
+
+        //long type
+        long l = 20;
+        System.out.println("l = "+l);
+
+        //float type
+        float f = 20.25f;
+        System.out.println("f = "+f);
+
+        //double type
+        double d = 20.25;
+        System.out.println("d = "+d);
+
+        //char type
+        char ch = '&';
+        System.out.println("ch = "+ch);
+
+        //boolean type
+        boolean bool = true;
+        System.out.println("bool = "+b);
+    }
+}
