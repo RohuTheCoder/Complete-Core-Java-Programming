7e6a450fbb854d7eeff7496974f77e7a7a5f1dc4
Rohini G B <gbrohini840@gmail.com>
2024-11-15 20:10:35 +0530

    Arrays in Java

---
+++ Arrays in Java/ArraysasListExample.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysasListExample
+{
+    public static void main(String[] args) {
+        Integer intArr[] = { 10, 20, 15, 22, 35 };
+
+        // To convert the elements as List
+        System.out.println("Integer Array as List: "
+                +     Arrays.asList(intArr));
+
+    }
+}
